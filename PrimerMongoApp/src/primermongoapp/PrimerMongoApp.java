9403f81deb1e78519ea406477ad712a37264aeeb
Jonathan Ramriez <jonathan-ramirezz@hotmail.com>
2019-06-27 20:21:58 -0600

    .

--- PrimerMongoApp/src/primermongoapp/PrimerMongoApp.java
+++ PrimerMongoApp/src/primermongoapp/PrimerMongoApp.java
@@ -8,8 +8,11 @@ package primermongoapp;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import org.bson.Document;
 
 /**
  *
@@ -20,15 +23,49 @@ public class PrimerMongoApp {
     private final static String HOST = "localhost";
     private final static int PORT = 27017;
 
+    public void createRow() {
+        try {
+            MongoClient mongoClient = new MongoClient(HOST, PORT);
+            MongoDatabase db = mongoClient.getDatabase("local");
+            MongoCollection<Document> coll = db.getCollection("users");
+            Document doc = new Document("name", "john").append("age", 25).append("phone", "321-654-987");
+            coll.insertOne(doc);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public void mostrarDatos() {
+        try {
+            MongoClient mongoClient = new MongoClient(HOST, PORT);
+            MongoDatabase db = mongoClient.getDatabase("local");
+            MongoCollection<Document> coll = db.getCollection("users");
+            MongoCursor<Document> cursor = coll.find().iterator();
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                System.out.println(doc.toJson());
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         try {
             MongoClient mongoClient = new MongoClient(HOST, PORT);
             // Now connect to the test database      
-            MongoDatabase db = mongoClient.getDatabase("test");
+            MongoDatabase db = mongoClient.getDatabase("local");
+            for (String name : mongoClient.listDatabaseNames()) {
+                System.out.println("name: " + name);
+            }
             System.out.println("Connect to database successfully ");
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
+
+        PrimerMongoApp mongoApp = new PrimerMongoApp();
+        mongoApp.createRow();
+        mongoApp.mostrarDatos();
     }
 
 }
